028d83d3ba66485ce7ced86e65c39b4bac5ea3f6
Indiamara Enes <indiamaraenes@gmail.com>
2022-04-28 13:20:40 -0300

    projeto calculadora inicial

---
+++ script.js
@@ -0,0 +1,108 @@
+var n1 = "0";
+var operacao = null;
+var n2 = ''
+
+function incluirDigito(digito) {
+
+    if (operacao !== null) {
+        n2 = n2 + digito;
+        mostrarNoDisplay(n2);
+    } else {
+        if (n1 === "0") {
+            n1 = digito;
+        } else {
+            n1 += digito;
+        }
+
+        mostrarNoDisplay(n1)
+    }
+}
+
+var clicadoEmIgual = false;
+
+function finalizarCalculo() {
+    clicadoEmIgual = true;
+    var resultado = calcular();
+    n1 = resultado;
+    mostrarNoDisplay(n1);
+    console.log('n1', n1, 'operacao', operacao, 'n2', n2)
+}
+
+
+function obterPorcento() {
+    if (!n2) {
+        n2 = '';
+        mostrarNoDisplay(n1);
+    } else {
+        var porcento = n1 * n2 / 100;
+        n2 = porcento;
+
+        mostrarNoDisplay(n2)
+    }
+
+}
+
+function calcular() {
+    var nCalculado = 0;
+    var _n1 = parseFloat(n1)
+    var _n2 = parseFloat(n2)
+
+    switch (operacao) {
+        case '+':
+            nCalculado = _n1 + _n2;
+            break;
+        case '-':
+            nCalculado = _n1 - _n2;
+            break;
+        case '*':
+            nCalculado = _n1 * _n2;
+            break;
+        case '/':
+            nCalculado = _n1 / _n2;
+            break;
+    }
+    return nCalculado;
+}
+
+function iniciarCalculo(simbolo) {
+    if (clicadoEmIgual) {
+        clicadoEmIgual = false;
+        n2 = '';
+    }
+
+    if (operacao === null || n2 === '') {
+        operacao = simbolo;
+    } else {
+        //ja tem n1, operacao e n2
+        var resultado = calcular();
+        n1 = resultado;
+        operacao = simbolo;
+        n2 = '';
+        mostrarNoDisplay(n1)
+    }
+}
+
+function incluirPonto() {
+    if (operacao && n2 === '') {
+        n2 = '0.';
+    } else if (operacao && n2) {
+        n2 += '.';
+    } else {
+        n1 += '.';
+    }
+}
+
+function limpar() {
+    n1 = '0';
+    operacao = null;
+    n2 = '';
+    mostrarNoDisplay(n1);
+}
+
+function mostrarNoDisplay(valor) {
+    var valorStr = valor.toString()
+    if (valorStr.length > 9) {
+        valorStr = parseFloat(valor).toExponential(5)
+    }
+    document.querySelector('#display').innerHTML = valorStr;
+}
